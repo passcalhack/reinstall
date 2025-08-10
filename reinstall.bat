ab89802423f84858210b2664bfd3f47cff5551ad
passcalhack <77136041+passcalhack@users.noreply.github.com>
2025-08-10 21:03:41 +0800

    Update reinstall.bat

--- reinstall.bat
+++ reinstall.bat
@@ -2,8 +2,8 @@
 mode con cp select=437 >nul
 setlocal EnableDelayedExpansion
 
-set confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-set confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
+set confhome=https://raw.githubusercontent.com/passcalhack/reinstall/main
+set confhome_cn=https://raw.githubusercontent.com/passcalhack/reinstall/main
 rem set confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 set pkgs=curl,cpio,p7zip,dos2unix,jq,xz,gzip,zstd,openssl,bind-utils,libiconv,binutils
@@ -238,3 +238,4 @@ for %%c in (%cmds_space%) do (
     )
 )
 exit /b 0
+
